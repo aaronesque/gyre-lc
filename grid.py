dc7f3d85e990e64ef6977c008f98a3b3b628da5d
aaronesque <alopez@astro.wisc.edu>
2020-07-01 11:18:03 -0500

    Added take_first_derivs(self, nbr_1, nbr_2, 'axis'). Cross derivs next.

--- grid.py
+++ grid.py
@@ -205,30 +205,55 @@ class Grid:
         return stencil
 
 
-    def find_derivs (self, ij, show=False):
+    def lookup_neighbors (self, ij):
+
+        stencil = self.find_neighbors(ij)
+
+        nbrs = np.empty([3,3], dtype=object)
+
+        for nj in range(-1,2):
+
+            for ni in range(-1,2):
+
+                Teff_i = self.Teff_axis[ij[0]+ni]
+                logg_j = self.logg_axis[ij[1]+nj]
+
+                nbrs[ni+1,nj+1] = self.lookup(Teff_i,logg_j)
+
+        return nbrs
+
+
+    def take_first_deriv (self, node, neighbor_0, neighbor_1, axis):
+
+        x_0 = getattr(neighbor_0, axis)
+        x = getattr(node, axis)
+        x_1 = getattr(neighbor_1, axis)
+
+        I_0 = neighbor_0.data
+        I = node.data
+        I_1 = neighbor_1.data
+
+        a = np.abs( (x_1 - x)/(x_1 - x_0) )
+        b = np.abs( (x_0 - x)/(x_1 - x_0) )
+
+        return a*(I - I_0)/(x - x_0) + b*(I - I_1)/(x - x_1)
+
+
+    def find_first_derivs (self, ij, show=False):
   
-        Teff = self.Teff_axis[ij[0]]
-        logg = self.logg_axis[ij[1]]
+        i,j = ij
 
-        node = self.nodes[ij[0],ij[1]]
+        Teff = self.Teff_axis[i]
+        logg = self.logg_axis[j]
 
-        nbrs = self.find_neighbors(ij)
+        node = self.nodes[i,j]
 
-        for j in [-1,1]:
-
-            for i in [-1,1]:
-                
-                Teff_i = self.Teff_axis[ ij[0]+i ]
-                logg_j = self.logg_axis[ ij[1]+j ]
-
-                
-
-                
-                if show:
-                    if j==0 and i==0:  print(f'({nbrs[i+1,j+1]})', end=' ')
-                    else: print(nbrs[i+1,j+1], end=' ')
-            
-            if show: print('\n')
+        nbrs = self.lookup_neighbors(ij) 
+        
+        dIdTeff = self.take_first_deriv(node, nbrs[0][1], nbrs[2][1], 'Teff')        
+        dIdlogg = self.take_first_deriv(node, nbrs[1][0], nbrs[1][2], 'logg')    
+        
+        print(dIdTeff, dIdlogg)
 
             
 if __name__ == '__main__':
@@ -252,7 +277,7 @@ if __name__ == '__main__':
         user_i = 7 #int(input('Enter i=7 for Teff=30000K: '))
         user_j = 3 #int(input('Enter j=3 for logg=4.0: '))
         
-        grid.find_derivs((user_i,user_j), show=True)
+        grid.find_first_derivs((user_i,user_j), show=True)
 
     except IndexError:
         print("\nOpe, that right there's gonna be a problem. How's 'bout we try a different (Teff,logg)?\n")
