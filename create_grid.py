9db16f8271ce403e9dcc3764369f86c0bc1dbdc1
aaronesque <alopez@astro.wisc.edu>
2020-07-06 10:19:41 -0500

    set data_function() to Teff**2 * logg**2

--- create_grid.py
+++ create_grid.py
@@ -11,6 +11,14 @@ import numpy as np
 Teffs = [2500., 5000., 7500., 10000., 15000., 20000., 25000., 30000., 40000., 50000.]
 loggs = [2.5, 3.0, 3.5, 4.0, 4.5]
 
+# Create function for testing
+
+def data_function(Teff, logg):
+
+    #return random.random()
+
+    return (Teff**2)*(logg**2)
+
 # Create nodes
 
 for Teff in Teffs:
@@ -29,7 +37,7 @@ for Teff in Teffs:
             f.attrs['Teff'] = Teff
             f.attrs['logg'] = logg
 
-            data_list = [random.random()]
+            data_list = [data_function(Teff,logg)]
 
             f.create_dataset('data', data=np.array(data_list))
 
