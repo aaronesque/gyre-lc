b7d87a1c5ffc4c194b034050f3f701a6942eebda
aaronesque <alopez@astro.wisc.edu>
2020-09-03 09:39:15 -0500

    removed irrad parts for future debugging

--- obs_func.py
+++ obs_func.py
@@ -20,64 +20,7 @@ class observer:
         phi = (90-omega)/180 * np.pi
         
         return theta, phi
-
-    
-    def solve_true_anom (t, omega_orb, e):
-    
-        #def convert_t_to_phase
-    
-        def eval_true_anom (t, omega_orb, e):
-    
-            return lambda f : omega_orb*t \
-                - 2.*np.arctan((1.-e)*np.tan(f/2)/np.sqrt(1.-e**2)) \
-                + e*np.sqrt(1.-e**2)*np.sin(f)/(1.+e*np.cos(f))
-    
-        soln = np.array([fsolve(eval_true_anom(tau,omega_orb,e), tau%np.pi) for tau in t])
-    
-        return soln
-    
-    
-    def eval_ramp_func (self, l, m, mu):
-        
-        term1 = 2 * np.sqrt((2*l+1)/(4*np.pi))
-        
-        term2 = np.sqrt( np.math.factorial(l-m)/np.math.factorial(l+m) )
-        
-        term3 = np.cos(m*np.pi/2)/(1-m**2)
-        
-        # Can I do this integral using symbolic mu,
-        
-        term4 = integrate(lambda mu: np.sqrt(1-mu**2)*lpmv(m,l, mu), -1, 1)
-        
-        # or should I stick to integrating only those mu which I have data for?
-        
-        Z_lm = term1*term2*term3*term4
-        
-        return Z_lm
-    
-    
-    def eval_di_factor (self, x, l):
-        
-        atm = self.atm_data
-        
-        # I still don't know if I need a bandpass correction, or if defining b_l
-        # using the bandpass corrected intensities (as I do here) is sufficient.
-    
-        b_l = atm.data[f'I_{x}_{l}'][:]/atm.data['I_{x}_0'][:]
-    
-        return b_l
-    
-    
-    #def eval_irrad (self):
-    
-        #for l in range(2, n_l+1):
-            #for m in range(-n_l, n_l+1):
-        
-            #dJ/unperturbed_observed_flux 
-            # = bandpass_correction_coefficient*(L2/L1)*(R1/D(t))**2
-            #   * disc_integral_factor_(Burkart_used_Eddington_limb_darkening)
             
-        
     
     def eval_fourier (self, x, inc, omega):
         
@@ -110,9 +53,9 @@ class observer:
 
                     Y_lm = sph_harm(m, l, phi, theta)
                     
-                    R_xlm = atm.R_xl[x][l]
-                    T_xlm = atm.T_xl[x][l]
-                    G_xlm = atm.T_xl[x][l]
+                    R_xl = atm.R_xl[x][i_l]
+                    T_xl = atm.T_xl[x][i_l]
+                    G_xl = atm.T_xl[x][i_l]
                 
                     if k == 0:
                         if m == 0:
@@ -126,7 +69,7 @@ class observer:
     
                     # Add the Fourier contribution * spherical harmonic
                     
-                    A[i_k] += 2*kappa*(dR_lmk*R_xlm + dT_lmk*T_xlm + dG_lmk*G_xlm)*Y_lm
+                    A[i_k] += 2*kappa*(dR_lmk*R_xl + dT_lmk*T_xl + dG_lmk*G_xl)*Y_lm
         
         # Return data
         
