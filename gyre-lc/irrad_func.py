245298a7fa2e3db96ed82530ea9c70aa9e03bfb8
aaronesque <alopez@astro.wisc.edu>
2020-09-14 11:06:26 -0500

    eval_irrad works for L1->L2->observer

--- gyre-lc/irrad_func.py
+++ gyre-lc/irrad_func.py
@@ -1,70 +1,116 @@
 import numpy as np
 import h5py
 from scipy.integrate import quad as integrate
-from scipy.special import legendre, lpmv, sph_harm
-import f90nml as nml
+from scipy.special import lpmv, sph_harm
 
 ### Class definition
 
 class irradiation:
     
-    def __init__ (self, resp_data, atm_data):
+    def __init__ (self, atm_data, resp_data, bin_data, filter_x):
     
-        self.resp_data = resp_data
         self.atm_data = atm_data
-        #self.binary_data = binary_data
+        self.resp_data = resp_data
+        self.x = filter_x
         
-    
-    def solve_true_anom (t, omega_orb, e):
-    
-        #def convert_t_to_phase
-    
-        def eval_true_anom (t, omega_orb, e):
-    
-            return lambda f : omega_orb*t \
-                - 2.*np.arctan((1.-e)*np.tan(f/2)/np.sqrt(1.-e**2)) \
-                + e*np.sqrt(1.-e**2)*np.sin(f)/(1.+e*np.cos(f))
-    
-        soln = np.array([fsolve(eval_true_anom(tau,omega_orb,e), tau%np.pi) for tau in t])
-    
-        return soln
-    
-    
-    def eval_ramp_func (self, l, m, mu):
+        self.omega_orb = bin_data.omega_orb
+        self.e = bin_data.e
+        self.a = bin_data.a
         
-        term1 = 2 * np.sqrt((2*l+1)/(4*np.pi))
+        self.L1 = bin_data.L1
+        self.R1 = bin_data.R1
         
-        term2 = np.sqrt( np.math.factorial(l-m)/np.math.factorial(l+m) )
+        self.L2 = bin_data.L2
+        self.R2 = bin_data.R2
         
-        term3 = np.cos(m*np.pi/2)/(1-m**2)
+            
+            
+    def eval_ramp (self, l, m):
+        
+        term1 = 2* np.sqrt( ((2*l+1)/(4*np.pi)) \
+                           *(np.math.factorial(l-m)/np.math.factorial(l+m)) )
+        
+        if (np.abs(m)==1):
+            term2 = np.pi/2
+        else:
+            term2 = np.cos(m*np.pi/2)/(1-m**2)
         
         # Can I do this integral using symbolic mu,
         
-        term4 = integrate(lambda mu: np.sqrt(1-mu**2)*lpmv(m,l, mu), -1, 1)
+        term3, term3_err = integrate(lambda mu: np.sqrt(1-mu**2)*lpmv(m,l, mu), -1, 1)
         
         # or should I stick to integrating only those mu which I have data for?
-        
-        Z_lm = term1*term2*term3*term4
+        Z_lm = term1*term2*term3
         
         return Z_lm
     
     
-    def eval_di_factor (self, x, l):
+    def find_disk_intg_factor (self, l):
         
         atm = self.atm_data
         
         # I still don't know if I need a bandpass correction, or if defining b_l
         # using the bandpass corrected intensities (as I do here) is sufficient.
     
-        b_l = atm.data[f'I_{x}_{l}'][:]/atm.data['I_{x}_0'][:]
+        b_l = atm.data[f'I_{self.x}_{l}'][:]/atm.data[f'I_{self.x}_0'][:]
     
-        return b_l
+        return b_l[0]
+        
+    
+    def find_mean_anom (self, t, t_peri=0):
+        
+        return self.omega_orb*(t - t_peri)
+        
+    
+    def find_ecce_anom (self, M):
+    
+        Keppler = lambda E : E - self.e*np.sin(E) - M
+        
+        return fsolve(Keppler, 0)[0]
+        
+    
+    def find_true_anom (self, E):
+    
+        return 2*np.arctan( ((1+self.e)/(1-self.e))*np.tan(E/2) )
     
     
-    #def eval_irrad (self):
+    def convert_t_to_f (self, t, t_peri=0):
     
-        #for l in range(2, n_l+1):
-            #for m in range(-n_l, n_l+1):
+        M = self.find_mean_anom(t, t_peri)
+        
+        E = self.find_ecce_anom(M)
+        
+        f = self.find_true_anom(E)
+        
+        return f
+    
+    
+    def find_bin_sep (self, t, t_peri=0):
+        
+        f = self.convert_t_to_f(t, t_peri)#* 2*np.pi
+        
+        D = self.a*(1-self.e**2)/(1+self.e*np.cos(f))
+        
+        return D
+    
+    
+    def eval_irrad (self, t, t_peri=0):
+        
+        Dt = self.find_bin_sep(t, t_peri)
+        
+        rel_dJ = np.zeros_like(t)
+        
+        n_l = self.resp_data.data['l_max']
+        
+        for l in range(2, n_l+1):
+            for m in range(-l, l+1):
+                
+                Z_lm = self.eval_ramp(l, m)
+                b_l = self.find_disk_intg_factor(l)
+                #rel_dJ += b_l*Z_lm*(self.L2/self.L1)*(self.R1/Dt)**2
+                rel_dJ += b_l*Z_lm*(self.L1/self.L2)*(self.R2/Dt)**2
+        
+        return rel_dJ
         
             #dJ/unperturbed_observed_flux 
             # = bandpass_correction_coefficient*(L2/L1)*(R1/D(t))**2
