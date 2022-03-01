5f47174e98612d0c841c177837e065f47b3ad2f4
aaronesque <alopez@astro.wisc.edu>
2022-03-01 15:33:34 -0600

    reverting change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,6 @@ import sys
 
 sys.path.insert(0, os.path.abspath('exts'))
 sys.path.insert(0, os.path.abspath('../../lib/'))
-import gyrelc
 
 # -- Project information -----------------------------------------------------
 
