cc399ba9e9982820f61c26ff6868d3e9c34d2e8d
aaronesque <alopez@astro.wisc.edu>
2022-03-01 15:39:24 -0600

    renamed lib/ to gyrelc/

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath('exts'))
-sys.path.insert(0, os.path.abspath('../../lib/'))
+sys.path.insert(0, os.path.abspath('../../gyrelc/'))
 
 # -- Project information -----------------------------------------------------
 
